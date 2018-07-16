f54e29bbb97c870241f1775edc1e5a0abf6fdd13
Aimee Ukasick <aimeeu.opensource@gmail.com>
2018-07-16 09:45:55 -0500

    Initial commit of skeleton project
    Issue-ID: ACUMOS-1374
    
    Change-Id: I2812f25d4ffe8766bfb2af9782f9358bd57f465b
    Signed-off-by: Aimee Ukasick <aimeeu.opensource@gmail.com>

---
+++ src/test/java/org/acumos/securityverification/securityverification/SecurityVerificationApplicationTests.java
@@ -0,0 +1,16 @@
+package org.acumos.securityverification.securityverification;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecurityVerificationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
