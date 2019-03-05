32499ac0f2c45819b4919e1c87873f6a46cbe332
Justin Early <justin@avantsoft.com>
2019-03-05 14:11:17 -0800

    License Manager Library
    - Restructured security-verification repo license-manager library / security-verification service
    - Added placeholder license-manager library code and example test case.
    - Setup maven projects and using parent pom
    - Added sequence diagram for license manager
    
    Issue-ID: ACUMOS-2546
    Issue-ID: ACUMOS-2606
    
    Change-Id: I6e5529773ae856b1823850ee8bd44a1f8e2c2db2
    Signed-off-by: Justin Early <justin@avantsoft.com>

---
+++ license-manager/src/main/java/org/acumos/licensemanager/service/LicenseService.java
@@ -0,0 +1,76 @@
+/*-
+* ===============LICENSE_START=======================================================
+* Acumos Apache-2.0
+* ===================================================================================
+* Copyright (C) 2019 Ericsson Software Technology AB. All rights reserved.
+* ===================================================================================
+* This Acumos software file is distributed by AT&T and Tech Mahindra
+* under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* This file is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ===============LICENSE_END=========================================================
+*/ 
+package org.acumos.licensemanager.service;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Service;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+@EnableConfigurationProperties(LicenseClientProperties.class)
+public class LicenseService {
+
+    private final LicenseClientProperties serviceProperties;
+
+    public LicenseService(LicenseClientProperties serviceProperties) {
+        this.serviceProperties = serviceProperties;
+    }
+
+    // TODO use RightToUseController.getRightToUsesForSolAndUser()
+    public CompletableFuture<LicenseResponse> verifyRTU(LicenseRequest request) {
+        // currently only using workflow for dummy response
+        if (!this.serviceProperties.getCDS()) {
+
+            // for each workflow requested // need to get rtuid -- TODO cache this later
+            //
+            LicenseResponse response = new LicenseResponse();
+
+            for (String workflow : request.getWorkflow()) {
+                // processEachWorkflow(workflow);
+                // we will get all rtu for user and solution id from CDS fake that here
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                switch (workflow) {
+                    case "download":
+                        response.addWorkflow(workflow, this.serviceProperties.isDownloadAllowed());
+                        break;
+                    case "deploy":
+                        response.addWorkflow(workflow, this.serviceProperties.isDeployAllowed());
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return CompletableFuture.completedFuture(response);
+
+        }
+        return null;
+        // }else{
+        // // TODO
+
+        // }
+    }
+
+}
