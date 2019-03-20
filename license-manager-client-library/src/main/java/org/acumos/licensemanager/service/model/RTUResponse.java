d25d0560e619ba0ccdaa7d7200a7230484c8d3f8
justin14 <justin.early@est.tech>
2019-03-20 20:56:37 +0000

    CDS Support for verifyRTU
    - Replace mock api with CDS client calls to verify RTU
    - Replace test case with CDS mock objects and mock client
    - In mock test we check if RTU reference exists then allow download or deploy
    - Removing CompletableFuture since calls are blocking request calls
    - Add createRTU method to support creation of rtu for solution+user
    - Added support for mvn goal license:* for checking and auto adding license
    - Fixed up documentation formatting
    
    Change-Id: Ifa7eca65ea0fb15c9694151ebd136c28d63757c0
    Issue-ID: ACUMOS-2614, ACUMOS-2631
    Signed-off-by: justin14 <justin.early@est.tech>

---
+++ license-manager-client-library/src/main/java/org/acumos/licensemanager/service/model/RTUResponse.java
@@ -0,0 +1,100 @@
+/*-
+ * ===============LICENSE_START=======================================================
+ * Acumos Apache-2.0
+ * ===================================================================================
+ * Copyright (C) 2019 Ericsson Software Technology AB. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T and Tech Mahindra
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+
+package org.acumos.licensemanager.service.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.acumos.cds.domain.MLPRightToUse;
+
+public class RTUResponse implements IErrorResponse{
+
+  CreateRTURequest request;
+  boolean created;
+  boolean updated;
+  List<MLPRightToUse> rtu = new ArrayList<MLPRightToUse>();
+  List<Exception> rtuException = new ArrayList<Exception>();
+
+  public RTUResponse() {
+  }
+
+  /**
+   * @return the request
+   */
+  public CreateRTURequest getRequest() {
+    return request;
+  }
+
+  /**
+   * @param set the original create request
+   */
+  public void setRequest(CreateRTURequest request) {
+    this.request = request;
+  }
+
+  /**
+   * @return if rtu was created
+   */
+  public boolean isCreated() {
+    return created;
+  }
+
+  /**
+   * @param created indicate that rtu was created
+   */
+  public void setCreated(boolean created) {
+    this.created = created;
+  }
+
+  /**
+   * @return if rtu was updated instead of created
+   */
+  public boolean isUpdated() {
+    return updated;
+  }
+
+  /**
+   * @param updated indicate that rtu was updated
+   */
+  public void setUpdated(boolean updated) {
+    this.updated = updated;
+  }
+
+  public void addRtu(MLPRightToUse rightToUse) {
+    rtu.add(rightToUse);
+  }
+
+  public List<MLPRightToUse> getRtus() {
+    return rtu;
+  }
+
+  public void addError(Exception ex) {
+    rtuException.add(ex);
+  }
+
+  /**
+   * @return the rtuException
+   */
+  public List<Exception> getRtuException() {
+    return rtuException;
+  }
+
+}
--- license-manager-client-library/src/test/java/org/acumos/licensemanager/service/LicenseClientTest.java
+++ license-manager-client-library/src/test/java/org/acumos/licensemanager/service/LicenseClientTest.java
@@ -1,49 +1,159 @@
 /*-
-* ===============LICENSE_START=======================================================
-* Acumos Apache-2.0
-* ===================================================================================
-* Copyright (C) 2019 Ericsson Software Technology AB. All rights reserved.
-* ===================================================================================
-* This Acumos software file is distributed by AT&T and Tech Mahindra
-* under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* This file is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-* ===============LICENSE_END=========================================================
-*/ 
+ * ===============LICENSE_START=======================================================
+ * Acumos Apache-2.0
+ * ===================================================================================
+ * Copyright (C) 2019 Ericsson Software Technology AB. All rights reserved.
+ * ===================================================================================
+ * This Acumos software file is distributed by AT&T and Tech Mahindra
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ===============LICENSE_END=========================================================
+ */
+
 package org.acumos.licensemanager.service;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import java.util.Random;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+import org.acumos.cds.client.CommonDataServiceRestClientMockImpl;
+import org.acumos.cds.domain.MLPRightToUse;
+import org.acumos.cds.domain.MLPRtuReference;
+import org.acumos.cds.domain.MLPSolution;
+import org.acumos.cds.domain.MLPSolutionRevision;
+import org.acumos.cds.domain.MLPUser;
+import org.acumos.licensemanager.service.model.CreateRTURequest;
+import org.acumos.licensemanager.service.model.LicenseResponse;
+import org.acumos.licensemanager.service.model.RTUResponse;
+import org.acumos.licensemanager.service.model.VerifyLicenseRequest;
 import org.junit.Test;
 
-
+/**
+ * License Manager Client Unit tests
+ * - Test creation of the RTU and RTU references
+ * - Test reading and verifying license/right to use exists.
+ */
 public class LicenseClientTest {
 
-    @Test
-    public void contextLoads() throws InterruptedException, ExecutionException {
-        // Temporary values will be replaced with CDS in Boreas S4
-        LicenseClientProperties properties = new LicenseClientProperties();
-        properties.setDeployAllowed(false);
-        properties.setDownloadAllowed(true);
-        // Start usage of license service
-        LicenseService licenseSrvc = new LicenseService(properties);
-        VerifyLicenseRequest licenseDownloadRequest = new VerifyLicenseRequest(new String[]{"deploy","download"}, "dummysolutionid", "dummyuserid");
-        CompletableFuture<LicenseResponse> verifyUserRTU = licenseSrvc.verifyRTU(licenseDownloadRequest);
-        CompletableFuture.allOf(verifyUserRTU).join();
-        assertEquals(true, verifyUserRTU != null);
-        assertEquals(true, verifyUserRTU.get().getAllowedToUse().get("download").booleanValue());
-        assertEquals(false, verifyUserRTU.get().getAllowedToUse().get("deploy").booleanValue());
+	@Test
+	public void rightToUseReadTest() throws InterruptedException, ExecutionException {
 
-    }
+		CommonDataServiceRestClientMockImpl client = new CommonDataServiceRestClientMockImpl("url", "user", "pass");
+
+		// mock user for rtu test
+
+		MLPUser user = new MLPUser();
+		client.setLoginUser(user);
+		client.setUserById(user);
+		client.setUser(user);
+
+		// mock solution to use for rtu test
+		MLPSolution solution = new MLPSolution();
+		String solutionId = UUID.randomUUID().toString();
+		solution.setSolutionId(solutionId);
+		client.setSolution(solution);
+		client.setSolutionById(solution);
+
+		// mock revision for solution
+		MLPSolutionRevision solRev = new MLPSolutionRevision();
+		client.setSolutionRevisionById(solRev);
+		client.setSolutionRevision(solRev);
+
+		// generic right to use
+		Long rtuId = new Random().nextLong();
+		MLPRightToUse rightToUse = new MLPRightToUse(solutionId, true);
+		rightToUse.setRtuId(rtuId);
+
+		Set<MLPRtuReference> rtuReferences = new HashSet<MLPRtuReference>();
+		rtuReferences.add(new MLPRtuReference(UUID.randomUUID().toString()));
+		rightToUse.setRtuReferences(rtuReferences);
+		client.createRightToUse(rightToUse);
+
+		List<MLPRightToUse> rightToUseList = new ArrayList<MLPRightToUse>();
+		rightToUseList.add(rightToUse);
+		client.setRightToUseList(rightToUseList);
+
+		LicenseService licenseSrvc = new LicenseService(client);
+		VerifyLicenseRequest licenseDownloadRequest = new VerifyLicenseRequest(new String[] { "deploy", "download" },
+				"dummysolutionid", "dummyuserid");
+		LicenseResponse verifyUserRTU = licenseSrvc.verifyRTU(licenseDownloadRequest);
+		// CompletableFuture.allOf(verifyUserRTU).join();
+		assertEquals(true, verifyUserRTU != null);
+		assertEquals(true, verifyUserRTU.getAllowedToUse().get("download").booleanValue());
+		assertEquals(true, verifyUserRTU.getAllowedToUse().get("deploy").booleanValue());
+
+		client.setRightToUseList(new ArrayList<MLPRightToUse>());
+
+		licenseDownloadRequest = new VerifyLicenseRequest(new String[] { "deploy", "download" }, "dummysolutionid",
+				"dummyuserid");
+		verifyUserRTU = licenseSrvc.verifyRTU(licenseDownloadRequest);
+		assertEquals(true, verifyUserRTU != null);
+		assertEquals(false, verifyUserRTU.getAllowedToUse().get("download").booleanValue());
+		assertEquals(false, verifyUserRTU.getAllowedToUse().get("deploy").booleanValue());
+
+	}
+
+	@Test
+	public void createRightToUse() throws InterruptedException, ExecutionException {
+		// URL url = new URL("http", hostname, port, contextPath);
+		// logger.info("createClient: URL is {}", url);
+		CommonDataServiceRestClientMockImpl client = new CommonDataServiceRestClientMockImpl("url", "user", "pass");
+		String userId = "dummyuserid";
+
+		// mock user for rtu test
+		MLPUser user = new MLPUser();
+		client.setLoginUser(user);
+		client.setUserById(user);
+		client.setUser(user);
+
+		// mock solution to use for rtu test
+		MLPSolution solution = new MLPSolution();
+		String solutionId = UUID.randomUUID().toString();
+		solution.setSolutionId(solutionId);
+		client.setSolution(solution);
+		client.setSolutionById(solution);
+
+		// mock revision for solution
+		MLPSolutionRevision solRev = new MLPSolutionRevision();
+		client.setSolutionRevisionById(solRev);
+		client.setSolutionRevision(solRev);
+
+		// create
+		LicenseService licenseSrvc = new LicenseService(client);
+		CreateRTURequest licenseDownloadRequest = new CreateRTURequest(	solutionId, userId);
+		RTUResponse verifyUserRTU = licenseSrvc.createRTU(licenseDownloadRequest);
+		assertEquals(true, verifyUserRTU != null);
+		assertEquals(true, verifyUserRTU.isCreated());
+		assertEquals(false, verifyUserRTU.isUpdated());
+		assertEquals(solutionId, verifyUserRTU.getRequest().getSolutionId());
+		List<MLPRightToUse> rtus = verifyUserRTU.getRtus();
+
+		client.setRightToUseList(rtus);
+		// update
+		CreateRTURequest licenseDownloadRequest2 = new CreateRTURequest(	solutionId, userId);
+		RTUResponse verifyUserRTU2 = licenseSrvc.createRTU(licenseDownloadRequest2);
+		assertEquals(true, verifyUserRTU2 != null);
+		assertEquals("expect rtu is updated not created", false, verifyUserRTU2.isCreated());
+		assertEquals(true, verifyUserRTU2.isUpdated());
+		assertEquals(solutionId, verifyUserRTU2.getRequest().getSolutionId());
+		assertEquals(true, client.getRightToUses(solutionId, userId).size()>0);
+	
+	}
 
 }
