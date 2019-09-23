5da2a99b2baf459659ce88a2dc85893408c6eedf
Bryan Sullivan <bryan.sullivan@att.com>
2019-09-23 11:15:45 -0700

    Security Verification 1.1.0 - jenkins and rtu
    - Code formatting clean up
    - Licensing RTU check updates
    - Relocate/update scripts for Jenkins.
    - Scan invokes Jenkins job.
    - ScanResult handling from Jenkins.
    
    Issue-ID: ACUMOS-3428, ACUMOS-3125
    
    Change-Id: Ib3c7b28e9a2ca8662eec0ffaadda1f70403057cd
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>
    Signed-off-by: justin14 <justin.early@est.tech>
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>

--- security-verification-client-library/src/main/java/org/acumos/securityverification/utils/SVConstants.java
+++ security-verification-client-library/src/main/java/org/acumos/securityverification/utils/SVConstants.java
@@ -3,14 +3,15 @@
  * Acumos
  * ===================================================================================
  * Copyright (C) 2019 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+ * Modifications Copyright (C) 2019 Nordix Foundation.
  * ===================================================================================
  * This Acumos software file is distributed by AT&T and Tech Mahindra
  * under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * This file is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -22,51 +23,51 @@ package org.acumos.securityverification.utils;
 
 public class SVConstants {
 
-	// Path components
-	public static final String APPLICATION_JSON="application/json";
-	public static final String SECURITY_SCAN = "/scan/solution/{solutionId}/revision/{revisionId}";
-	public static final String SITE_CONFIG_UPDATE= "/update/siteConfig/verification";
-	
-	//Site config Json tag
-	public static final String SITE_VERIFICATION_KEY = "verification";
-	public static final String VERIFICATION = "verification";
-	public static final String LICENSESCAN = "licenseScan";
-	public static final String CREATED = "created";
-	public static final String UPDATED = "updated";
+  // Path components
+  public static final String APPLICATION_JSON = "application/json";
+  public static final String SECURITY_SCAN = "/scan/solution/{solutionId}/revision/{revisionId}";
+  public static final String SITE_CONFIG_UPDATE = "/update/siteConfig/verification";
 
-	public static final String DEPLOY = "deploy";
-	public static final String DOWNLOAD = "download";
-	public static final String SHARE = "share";
-	public static final String PUBLISHCOMPANY = "publishCompany";
-	public static final String PUBLISHPUBLIC = "publishPublic";
+  // Site config Json tag
+  public static final String SITE_VERIFICATION_KEY = "verification";
+  public static final String VERIFICATION = "verification";
+  public static final String LICENSESCAN = "licenseScan";
+  public static final String CREATED = "created";
+  public static final String UPDATED = "updated";
+  public static final String RTUCHECK = "rtuCheck";
 
-	public static final String SECURITYSCAN = "securityScan";
-	public static final String LICENSEVERIFY = "licenseVerify";
-	public static final String SECURITYVERIFY = "securityVerify";
-	public static final String EXTERNALSCAN = "externalScan";
-	public static final String ALLOWEDLICENSE = "allowedLicense";
-	public static final String TYPE = "type";
-	public static final String VALUE = "value";
-	
-	//CDUMP Json tag
-	public static final String ARTIFACT_TYPE_CDUMP= "CD";
-	public static final String ARTIFACT_TYPE_SCANRESULT = "SR";
-	public static final String JSON_FILE_NAME= "ACUMOS-CDUMP.json";
-	public static final String MODEL_TYPE_CODE= "PR";
-	public static final String TOOL_KIT_TYPE_CODE= "CP";
-	
-	public static final String NODES="nodes";
-	public static final String DEPENDS_ON="depends_on";
-	public static final String CONTAINER_NAME="container_name";
-	public static final String IMAGE="image";
-	
-	public static final String REASON_NOT_FOUND = "solution/revision not found";
-	public static final String LICENSE_SCAN_INCOMPLETE = "license scan incomplete";
-	
-	public static final String SOLUTIONID = "solutionId";
-	public static final String REVISIONID = "revisionId";
-	public static final String WORKFLOWID = "workflowId";
-	public static final String FORWARD_SLASH = "/";
-	public static final String TRUE="true";
-	
+  public static final String DEPLOY = "deploy";
+  public static final String DOWNLOAD = "download";
+  public static final String SHARE = "share";
+  public static final String PUBLISHCOMPANY = "publishCompany";
+  public static final String PUBLISHPUBLIC = "publishPublic";
+
+  public static final String SECURITYSCAN = "securityScan";
+  public static final String LICENSEVERIFY = "licenseVerify";
+  public static final String SECURITYVERIFY = "securityVerify";
+  public static final String EXTERNALSCAN = "externalScan";
+  public static final String ALLOWEDLICENSE = "allowedLicense";
+  public static final String TYPE = "type";
+  public static final String VALUE = "value";
+
+  // CDUMP Json tag
+  public static final String ARTIFACT_TYPE_CDUMP = "CD";
+  public static final String ARTIFACT_TYPE_SCANRESULT = "SR";
+  public static final String JSON_FILE_NAME = "ACUMOS-CDUMP.json";
+  public static final String MODEL_TYPE_CODE = "PR";
+  public static final String TOOL_KIT_TYPE_CODE = "CP";
+
+  public static final String NODES = "nodes";
+  public static final String DEPENDS_ON = "depends_on";
+  public static final String CONTAINER_NAME = "container_name";
+  public static final String IMAGE = "image";
+
+  public static final String REASON_NOT_FOUND = "solution/revision not found";
+  public static final String LICENSE_SCAN_INCOMPLETE = "license scan incomplete";
+
+  public static final String SOLUTIONID = "solutionId";
+  public static final String REVISIONID = "revisionId";
+  public static final String USERID = "userId";
+  public static final String FORWARD_SLASH = "/";
+  public static final String TRUE = "true";
 }
--- security-verification-client-library/src/test/java/org/acumos/securityverification/utils/SecurityVerificationJsonParserTest.java
+++ security-verification-client-library/src/test/java/org/acumos/securityverification/utils/SecurityVerificationJsonParserTest.java
@@ -3,14 +3,15 @@
  * Acumos
  * ===================================================================================
  * Copyright (C) 2019 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+ * Modifications Copyright (C) 2019 Nordix Foundation.
  * ===================================================================================
  * This Acumos software file is distributed by AT&T and Tech Mahindra
  * under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * This file is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -29,7 +30,6 @@ import java.io.InputStreamReader;
 import java.lang.invoke.MethodHandles;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 import org.acumos.securityverification.domain.SecurityVerificationCdump;
 import org.json.simple.JSONObject;
 import org.junit.Before;
@@ -39,90 +39,108 @@ import org.slf4j.LoggerFactory;
 
 public class SecurityVerificationJsonParserTest {
 
-	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  private static final Logger logger =
+      LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-	private SecurityVerificationJsonParser securityVerificationJsonParser;
-	private String cdumpInputStreamResult;
-	private String siteConfigResult;
-	
-	@Before
-	public void setUp() throws Exception {
-		InputStream cdumpInputStream = SecurityVerificationJsonParserTest.class
-				.getResourceAsStream("/ACUMOS-CDUMP-BF0478BC-0D3F-4433-80B6-D7A0F6DB2DF1-1.json");
-		cdumpInputStreamResult = new BufferedReader(new InputStreamReader(cdumpInputStream)).lines().parallel()
-				.collect(Collectors.joining("\n"));
-		InputStream siteConfigInputStream = SecurityVerificationJsonParserTest.class.getResourceAsStream("/siteConfig.json");
-		siteConfigResult = new BufferedReader(new InputStreamReader(siteConfigInputStream)).lines().parallel()
-				.collect(Collectors.joining("\n"));
-		securityVerificationJsonParser = new SecurityVerificationJsonParser();
-	}
+  private SecurityVerificationJsonParser securityVerificationJsonParser;
+  private String cdumpInputStreamResult;
+  private String siteConfigResult;
 
-	@Test
-	public void testParseCdumpJsonFile() throws Exception{
-			SecurityVerificationCdump securityVerificationCdump = securityVerificationJsonParser
-					.parseCdumpJsonFile(cdumpInputStreamResult);
-			assertNotNull(securityVerificationCdump.getCid());
-			assertNotNull(securityVerificationCdump.getCname());
-			assertNotNull(securityVerificationCdump.getNodes());
-			assertNotNull(securityVerificationCdump.getSolutionId());
-			assertNotNull(securityVerificationCdump.getVersion());
-			assertEquals("bf0478bc-0d3f-4433-80b6-d7a0f6db2df1", securityVerificationCdump.getSolutionId());
-			assertEquals("1", securityVerificationCdump.getVersion());
-			assertEquals("IngestAdder", securityVerificationCdump.getCname());
-	}
+  @Before
+  public void setUp() throws Exception {
+    InputStream cdumpInputStream =
+        SecurityVerificationJsonParserTest.class.getResourceAsStream(
+            "/ACUMOS-CDUMP-BF0478BC-0D3F-4433-80B6-D7A0F6DB2DF1-1.json");
+    cdumpInputStreamResult =
+        new BufferedReader(new InputStreamReader(cdumpInputStream))
+            .lines()
+            .parallel()
+            .collect(Collectors.joining("\n"));
+    InputStream siteConfigInputStream =
+        SecurityVerificationJsonParserTest.class.getResourceAsStream("/siteConfig.json");
+    siteConfigResult =
+        new BufferedReader(new InputStreamReader(siteConfigInputStream))
+            .lines()
+            .parallel()
+            .collect(Collectors.joining("\n"));
+    securityVerificationJsonParser = new SecurityVerificationJsonParser();
+  }
 
-	@Test
-	public void testExternalScanValue() throws Exception{
-			JSONObject siteConfigDataJsonObj = new JSONObject();
-			siteConfigDataJsonObj = securityVerificationJsonParser.stringToJsonObject(siteConfigResult);
-			JSONObject siteConfigVerificationObject = (JSONObject) siteConfigDataJsonObj.get(SVConstants.VERIFICATION);
-			String externalScanObject = securityVerificationJsonParser.externalScanValue(siteConfigVerificationObject);
-			assertNotNull(externalScanObject);
-			assertFalse(false);
-	}
+  @Test
+  public void testParseCdumpJsonFile() throws Exception {
+    SecurityVerificationCdump securityVerificationCdump =
+        securityVerificationJsonParser.parseCdumpJsonFile(cdumpInputStreamResult);
+    assertNotNull(securityVerificationCdump.getCid());
+    assertNotNull(securityVerificationCdump.getCname());
+    assertNotNull(securityVerificationCdump.getNodes());
+    assertNotNull(securityVerificationCdump.getSolutionId());
+    assertNotNull(securityVerificationCdump.getVersion());
+    assertEquals("bf0478bc-0d3f-4433-80b6-d7a0f6db2df1", securityVerificationCdump.getSolutionId());
+    assertEquals("1", securityVerificationCdump.getVersion());
+    assertEquals("IngestAdder", securityVerificationCdump.getCname());
+  }
 
-	@Test
-	public void testAllowedLicenseMap() throws Exception {
-			JSONObject siteConfigDataJsonObj = new JSONObject();
-			siteConfigDataJsonObj = securityVerificationJsonParser.stringToJsonObject(siteConfigResult);
-			JSONObject siteConfigVerificationObject = (JSONObject) siteConfigDataJsonObj.get(SVConstants.VERIFICATION);
-			Map<String, String> allowedLicenseObjectMap = securityVerificationJsonParser
-					.allowedLicenseMap(siteConfigVerificationObject);
-			assertNotNull(allowedLicenseObjectMap);
-	}
+  @Test
+  public void testExternalScanValue() throws Exception {
+    JSONObject siteConfigDataJsonObj = new JSONObject();
+    siteConfigDataJsonObj = securityVerificationJsonParser.stringToJsonObject(siteConfigResult);
+    JSONObject siteConfigVerificationObject =
+        (JSONObject) siteConfigDataJsonObj.get(SVConstants.VERIFICATION);
+    String externalScanObject =
+        securityVerificationJsonParser.externalScanValue(siteConfigVerificationObject);
+    assertNotNull(externalScanObject);
+    assertFalse(false);
+  }
 
-	@Test
-	public void testSiteConfigMap() throws Exception{
-			JSONObject siteConfigDataJsonObj = new JSONObject();
-			siteConfigDataJsonObj = securityVerificationJsonParser.stringToJsonObject(siteConfigResult);
-			JSONObject siteConfigVerificationObject = (JSONObject) siteConfigDataJsonObj.get(SVConstants.VERIFICATION);
-			Map<String, String> licenseScan = securityVerificationJsonParser.siteConfigMap(siteConfigDataJsonObj,
-					SVConstants.LICENSESCAN);
-			Map<String, String> securityScan = securityVerificationJsonParser.siteConfigMap(siteConfigDataJsonObj,
-					SVConstants.SECURITYSCAN);
-			Map<String, String> licenseVerify = securityVerificationJsonParser.siteConfigMap(siteConfigDataJsonObj,
-					SVConstants.LICENSEVERIFY);
-			Map<String, String> securityVerify = securityVerificationJsonParser.siteConfigMap(siteConfigDataJsonObj,
-					SVConstants.SECURITYVERIFY);
-			assertNotNull(licenseScan);
-			assertNotNull(securityScan);
-			assertNotNull(licenseVerify);
-			assertNotNull(securityVerify);
-	}
+  @Test
+  public void testAllowedLicenseMap() throws Exception {
+    JSONObject siteConfigDataJsonObj = new JSONObject();
+    siteConfigDataJsonObj = securityVerificationJsonParser.stringToJsonObject(siteConfigResult);
+    JSONObject siteConfigVerificationObject =
+        (JSONObject) siteConfigDataJsonObj.get(SVConstants.VERIFICATION);
+    Map<String, String> allowedLicenseObjectMap =
+        securityVerificationJsonParser.allowedLicenseMap(siteConfigVerificationObject);
+    assertNotNull(allowedLicenseObjectMap);
+  }
 
-	@Test
-	public void testStringToJsonObject() throws Exception {
-			JSONObject siteConfigDataJsonObj = new JSONObject();
-			siteConfigDataJsonObj = securityVerificationJsonParser.stringToJsonObject(siteConfigResult);
-			assertNotNull(siteConfigDataJsonObj);
-	}
+  @Test
+  public void testSiteConfigMap() throws Exception {
+    JSONObject siteConfigDataJsonObj = new JSONObject();
+    siteConfigDataJsonObj = securityVerificationJsonParser.stringToJsonObject(siteConfigResult);
+    JSONObject siteConfigVerificationObject =
+        (JSONObject) siteConfigDataJsonObj.get(SVConstants.VERIFICATION);
+    Map<String, String> licenseScan =
+        securityVerificationJsonParser.siteConfigMap(
+            siteConfigDataJsonObj, SVConstants.LICENSESCAN);
+    Map<String, String> securityScan =
+        securityVerificationJsonParser.siteConfigMap(
+            siteConfigDataJsonObj, SVConstants.SECURITYSCAN);
+    Map<String, String> licenseVerify =
+        securityVerificationJsonParser.siteConfigMap(
+            siteConfigDataJsonObj, SVConstants.LICENSEVERIFY);
+    Map<String, String> securityVerify =
+        securityVerificationJsonParser.siteConfigMap(
+            siteConfigDataJsonObj, SVConstants.SECURITYVERIFY);
+    assertNotNull(licenseScan);
+    assertNotNull(securityScan);
+    assertNotNull(licenseVerify);
+    assertNotNull(securityVerify);
+  }
 
-	@Test
-	public void testScanResultRootLicenseType() throws Exception{
-			String scanResultJson = "{\"schema\": \"1.0\",\"verifiedLicense\": \"false\",\"reason\": \"no license.txt document found\",\"solutionId\": \"0357ff93-46f4-45df-a6f7-7f2cf5ea779f\",\"revisionId\": \"0ce6b633-0604-4a59-9ab2-bad78952c54a\",\"scanTime\": \"190416-143640\",\"root_license\": {\"name\": \"\",\"type\": \"SPDX\"}}";
-			SecurityVerificationJsonParser securityVerificationJsonParser = new SecurityVerificationJsonParser();
-			String type = securityVerificationJsonParser.scanResultRootLicenseType(scanResultJson);
-			assertNotNull(type);
-	}
+  @Test
+  public void testStringToJsonObject() throws Exception {
+    JSONObject siteConfigDataJsonObj = new JSONObject();
+    siteConfigDataJsonObj = securityVerificationJsonParser.stringToJsonObject(siteConfigResult);
+    assertNotNull(siteConfigDataJsonObj);
+  }
 
+  @Test
+  public void testScanResultRootLicenseType() throws Exception {
+    String scanResultJson =
+        "{\"schema\": \"1.0\",\"verifiedLicense\": \"false\",\"reason\": \"no license.txt document found\",\"solutionId\": \"0357ff93-46f4-45df-a6f7-7f2cf5ea779f\",\"revisionId\": \"0ce6b633-0604-4a59-9ab2-bad78952c54a\",\"scanTime\": \"190416-143640\",\"root_license\": {\"name\": \"\",\"type\": \"SPDX\"}}";
+    SecurityVerificationJsonParser securityVerificationJsonParser =
+        new SecurityVerificationJsonParser();
+    String type = securityVerificationJsonParser.scanResultRootLicenseType(scanResultJson);
+    assertNotNull(type);
+  }
 }
