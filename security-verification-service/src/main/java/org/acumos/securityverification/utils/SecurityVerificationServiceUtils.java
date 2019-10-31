ace82a7c1402a01f307b3086fda181a21041e3e4
Bryan Sullivan <bryan.sullivan@att.com>
2019-10-31 15:39:36 -0700

    Correct SV endpoint mapping, part 2
    Issue-ID: ACUMOS-3660
    
    Update SV scanning service to accept /scan as
    prefix to site-config update API, since updating
    Portal-BE for new SVCL version will not happen in
    the initial Clio release.
    
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>
    Change-Id: Ic9c9892325c8014ca76222d96935aa83b1507454
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>

--- security-verification-service/src/main/java/org/acumos/securityverification/utils/SecurityVerificationServiceUtils.java
+++ security-verification-service/src/main/java/org/acumos/securityverification/utils/SecurityVerificationServiceUtils.java
@@ -19,15 +19,8 @@
  */
 package org.acumos.securityverification.utils;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.invoke.MethodHandles;
-import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.env.Environment;
 
 public class SecurityVerificationServiceUtils {
 
@@ -41,108 +34,5 @@ public class SecurityVerificationServiceUtils {
       isEmpty = true;
     }
     return isEmpty;
-  }
-
-  public static byte[] executeScript(
-      String scriptFile, String solutionId, String revisionId, String folder, Environment env)
-      throws Exception {
-    logger.debug(
-        "Inside executeScript scriptFile:{} solutionId:{} revisionId:{}",
-        scriptFile,
-        solutionId,
-        revisionId);
-
-    String scriptFileName =
-        SVServiceConstants.FORWARD_SLASH
-            + SVServiceConstants.MAVEN
-            + SVServiceConstants.FORWARD_SLASH
-            + SVServiceConstants.SECURITY_SCAN
-            + SVServiceConstants.FORWARD_SLASH
-            + scriptFile;
-    String folderName =
-        SVServiceConstants.FORWARD_SLASH
-            + SVServiceConstants.MAVEN
-            + SVServiceConstants.FORWARD_SLASH
-            + SVServiceConstants.SECURITY_SCAN
-            + SVServiceConstants.FORWARD_SLASH
-            + folder;
-    byte[] result = null;
-    ProcessBuilder processBuilder = null;
-    Process process = null;
-    BufferedReader reader = null;
-    try {
-      StringBuilder sb = new StringBuilder();
-      String createFolder =
-          SVServiceConstants.MAVEN
-              + SVServiceConstants.FORWARD_SLASH
-              + SVServiceConstants.SECURITY_SCAN
-              + SVServiceConstants.FORWARD_SLASH
-              + folder;
-      logger.debug("Execute Command: mkdir {}", createFolder);
-      String[] cmd1 = {"mkdir", createFolder};
-      processBuilder = new ProcessBuilder(cmd1);
-      if (processBuilder != null) {
-        process = processBuilder.start();
-        int errCode = process.waitFor();
-        logger.debug(
-            "Cmd1 mkdir folder, Echo command executed, any errors? "
-                + (errCode == 0 ? "No" : "Yes"));
-        String line = null;
-        reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        while ((line = reader.readLine()) != null) {
-          sb.append(line + System.getProperty("line.separator"));
-        }
-        logger.debug("Cmd1>>  {}", sb.toString());
-      }
-
-      if (scriptFile.equalsIgnoreCase(SVServiceConstants.SCRIPTFILE_DUMP_MODEL)) {
-        logger.debug(
-            "Execute Command: bash {} {} {} {}",
-            scriptFileName,
-            solutionId,
-            revisionId,
-            folderName);
-
-        String[] dump_model_cmd = {"bash", scriptFileName, solutionId, revisionId, folderName};
-        processBuilder = new ProcessBuilder(dump_model_cmd);
-        logger.debug("Please wait scan is in-progress");
-        if (processBuilder != null) {
-          process = processBuilder.start();
-          int errCode = process.waitFor();
-          logger.debug(
-              "dump_model_cmd script, Echo command executed, any errors? "
-                  + (errCode == 0 ? "No" : "Yes"));
-          String line = null;
-          reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-          while ((line = reader.readLine()) != null) {
-            sb.append(line + System.getProperty("line.separator"));
-          }
-          logger.debug("dump_model_cmd>>  {}", sb.toString());
-        }
-        logger.debug("Scan result location: {}", folderName);
-      }
-
-    } finally {
-      if (null != process) {
-        process.waitFor(10, TimeUnit.SECONDS);
-        process.destroy();
-      }
-      if (null != reader) {
-        try {
-          reader.close();
-        } catch (IOException e) {
-          logger.error("executeScript failed {}", e);
-          throw e;
-        }
-      }
-    }
-
-    return result;
-  }
-
-  public static File readScanOutput(String path) {
-    logger.debug("Inside readScanOutput");
-    File file = new File(path);
-    return file;
   }
 }
